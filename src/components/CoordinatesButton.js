d200cc334640bf0ae5c936acd395a24758614cf7
Nahum <losond8@gmail.com>
2021-03-18 01:32:59 -0400

    React Events in Detail Lab - passing functions as props, Use of event Listeners in React, implementing setTimeout, e.presist

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends Component {
+    render() {
+
+        return (
+            <button onClick ={(event) => this.props.onReceiveCoordinates([event.clientX, event.clientY])}> Click me for Mouse Coordinates!</button>
+        )
+    }
+
+}
+
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,50 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+class DelayedButton extends Component {
+
+    //  handleClick = (event) => {
+    //     event.persist();
+    //     return setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+    // }
+
+    render() {
+
+        // return (
+        //     <button onClick ={(event) => this.handleClick(event)}> Click me!</button>
+        // )
+
+        // return (
+        //     <button onClick ={(event) => this.props.onDelayedClick(event)}> Click me!</button>
+        // )
+        
+        return (
+            <button onClick = {(event) => 
+                { console.log(event)
+                    event.persist();
+                 setTimeout( () => this.props.onDelayedClick(event), this.props.delay)}
+                }> Click me!</button>
+        )
+    }
+
+}
+
+
+export default DelayedButton;
+
+
+// setTimeout(() => { alert('Howdy!'); }, 2000);
+
+
+// class DelayedButton extends Component {
+//     render() {
+
+//         return (
+//             <button onClick = {(event) => 
+//                 { 
+//                 setTimeout(event => this.props.onDelayedClick(event)), this.props.delay}
+//                 }> Click me!</button>
+//         )
+//     }
+
+// }
